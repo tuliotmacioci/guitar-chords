008e3dd0afe869ad3c4c26ec1aa5f519747bfcc0
tmacioci <taveiramaciocitulio@gmail.com>
2026-09-11 02:13:31 -0300

    define notas
    naturais + sustenidos (sem bemol)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() { 
+    string notas[] = {
+        "A", "A#", "B", "C",
+        "C#", "D", "D#", "E",
+        "F", "F#", "G", "G#"
+    };
+
+    for (int i=0; i<12; i++) {
+        cout << notas[i] << endl;
+    }
+
+    // "x maior" = x, x+4, x+7 (tonica, terca aumentada, quinta justa)
+    cout << "Do maior => " << notas[3] << notas[3+4] << notas[3+7] << endl;
 
     return 0;
 }
