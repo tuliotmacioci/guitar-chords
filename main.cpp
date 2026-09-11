8a6aad29a808dcfcd07a14b27e0e147795eb9226
tmacioci <taveiramaciocitulio@gmail.com>
2026-09-11 02:19:36 -0300

    formacao de acordes maiores e menores

--- main.cpp
+++ main.cpp
@@ -8,12 +8,17 @@ int main() {
         "F", "F#", "G", "G#"
     };
 
+    /*
     for (int i=0; i<12; i++) {
         cout << notas[i] << endl;
     }
+    */
 
     // "x maior" = x, x+4, x+7 (tonica, terca aumentada, quinta justa)
-    cout << "Do maior => " << notas[3] << notas[3+4] << notas[3+7] << endl;
+    cout << "Do maior (C) => " << notas[3] << notas[3+4] << notas[3+7] << endl;
+
+    // "y menor" = y, y+3, y+7 (tonica, terca menor, quinta justa)
+    cout << "La menor (Am) => " << notas[0] << notas[0+3] << notas[0+7] << endl; 
 
     return 0;
 }
