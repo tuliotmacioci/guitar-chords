b314e7db0ce4473af1ff694fb5496978bd526dcd
tmacioci <taveiramaciocitulio@gmail.com>
2026-09-15 19:56:37 -0300

    fluxo de menus interligados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#pragma region ESCALAS
+
 void escalaMaior(string notas[12], int nota) {
     // tom - tom - semitom - tom - tom - tom - semitom
-    cout << "Escala maior => "
+    cout << "Maior => "
          << notas[nota] << ", "
          << notas[(nota+2)%12] << ", "
          << notas[(nota+4)%12] << ", "
@@ -16,7 +18,7 @@ void escalaMaior(string notas[12], int nota) {
 
 void escalaMenor(string notas[12], int nota) {
     // tom - semitom - tom - tom - semitom - tom - tom
-    cout << "Escala menor => "
+    cout << "Menor => "
          << notas[nota] << ", "
          << notas[(nota+2)%12] << ", "
          << notas[(nota+3)%12] << ", "
@@ -26,6 +28,10 @@ void escalaMenor(string notas[12], int nota) {
          << notas[(nota+10)%12] << ", "
          << notas[(nota+12)%12] << endl;
 }
+
+#pragma endregion
+
+#pragma region ACORDES
 
 void acordeMaior(string notas[12], int nota) {
     int tonica = nota;
@@ -49,6 +55,8 @@ void acordeMenor(string notas[12], int nota) {
          << notas[quinta_justa] << endl;
 }
 
+#pragma endregion
+
 int main() {
     string notas[12] = {
         "A", "Bb", "B", "C",
@@ -56,37 +64,42 @@ int main() {
         "F", "F#", "G", "Ab"
     };
 
-    int nota=0;
+    int opt1=0, opt2=0;
     do {
-        // cout << "0. Sair" << endl;
-        for (int i=0; i<12; i++) {
-            cout << i+1 << ". " << notas[i] << endl;
-        }
-        cout << "Escolha uma nota (1-12): ";
-        cin >> nota;
-        nota--;
-
-        int opt=0;
         do {
-            cout << "[" << notas[nota] << "]\n"
-                // << "0. Voltar" << endl
-                << "1. Escalas" << endl
-                << "2. Acordes" << endl
-                << "Escolha uma opcao (0-2): ";
-            cin >> opt;
-
-            switch (opt) {
-                case 1:
-                    escalaMaior(notas, nota);
-                    escalaMenor(notas, nota);
-                    break;
-                case 2:
-                    acordeMaior(notas, nota);
-                    acordeMenor(notas, nota);
-                default: break;
+            cout << " --------------------------\n";
+            for (int i=0; i<4; i++) {
+                cout << left;
+                cout << "| " <<setw(2)<< i+1 << ". " <<setw(3)<< notas[i];
+                cout << "| " <<setw(2)<< i+5 << ". " <<setw(3)<< notas[i+4];
+                cout << "| " <<setw(2)<< i+9 << ". " <<setw(3)<< notas[i+8];
+                cout << "|\n";
             }
-        } while (opt<=0 || opt>2);
-    } while (nota<0 || nota>11);
+            cout << " --------------------------\n"         
+                 << "> Escolha uma nota (1-12): ";
+            cin >> opt1;
+        } while (opt1<=0 || opt1>12);
+        int nota = opt1-1;
 
+        do {
+            cout << " -------------\n"
+                 << "| 0 . Voltar  |\n"
+                 << "| 1 . Escalas |\n"
+                 << "| 2 . Acordes |\n"
+                 << " -------------\n"
+                 << "> Escolha uma opcao (0-2): ";
+            cin >> opt2;
+
+            if (opt2==1) {
+                escalaMaior(notas, nota);
+                escalaMenor(notas, nota);
+            } 
+            else if (opt2==2) {
+                acordeMaior(notas, nota);
+                acordeMenor(notas, nota);
+            }
+        } while (opt2!=0);
+    } while (true);
+    
     return 0;
 }
